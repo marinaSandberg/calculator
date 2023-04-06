3902b6c179ae12b23d133e9c2a4b2b52a9ffe50c
marinaSandberg <marina_p13@hotmail.com>
2023-04-06 16:30:01 +0200

    layout

--- calculator.js
+++ calculator.js
@@ -1,19 +1,19 @@
-function calculate(fn) {
+function calculate() {
+
+    let expression = document.querySelector("#expression").value;
+    let result = document.querySelector("p");
 
     //If no expression provided show 0
     if (!expression) {
-        return 0;
+        result.textContent = 0;
     }
 
     //If no operations provided show last number
     if (!expression.match(/[+ && - && * && /]/)) {
-        return expression.slice(-1);
+        result.textContent = expression.slice(-1);
     }
 
     //Creates new function that executes the input expression
-    return new Function("return " + fn)();
+    let content = new Function("return " + expression)();
+    result.textContent = content;
 }
-  
-let expression = "5/5*7+1.5*2";
-
-console.log(calculate(expression)); // Output: 10
