8231b61562033839c471c0f33a9a1e382d296222
marinaSandberg <marina_p13@hotmail.com>
2023-04-26 19:40:08 +0200

    final , both calculators working

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
 var display = document.querySelector("#expression");
-var result = document.querySelector("p");
+var output = document.querySelector("p");
 
 function calculatorOne () {
 
@@ -12,71 +12,62 @@ function calculatorOne () {
     
         //If no expression provided show 0
         if (!expression) {
-            result.textContent = 0;
+            output.textContent = 0;
         }
     
         //If no operations provided show last number
         if (!expression.match(/[+ && - && * && /]/)) {
-            result.textContent = expression.slice(-1);
+            output.textContent = expression.slice(-1);
         }
     
         //Creates new function that executes the input expression
         let content = new Function("return " + expression)();
-        result.textContent = content;
+        output.textContent = content;
     }
 }
 
 var operand1 = '';
 var operand2 = '';
 var operator = '';
-let expression = [];
 result = null;
 
+// Function when clicking on numbers
 function appendDigit(digit) {
   if (operator === '') {
     operand1 += digit;
-    expression.push(operand1);
-    display.value = expression.toString();
+    display.value = operand1;
   } else {
     operand2 += digit;
-    expression.push(operand2);
-    display.value = expression.toString();
+    display.value = operand1 + operator + operand2;
   }
 }
 
-function compute(op) {
+// Function when clicking on operators
+function compute(operation) {
   if (operand2 !== '') {
     var num1 = parseFloat(operand1);
     var num2 = parseFloat(operand2);
     switch (operator) {
       case '+':
         result = num1 + num2;
-        expression.push(operator);
-        display.value = expression.toString();
         break;
       case '-':
         result = num1 - num2;
-        expression.push(operator);
-        display.value = expression.toString();
         break;
       case '*':
         result = num1 * num2;
-        expression.push(operator);
-        display.value = expression.toString();
         break;
       case '/':
         result = num1 / num2;
-        expression.push(operator);
-        display.value = expression.toString();
         break;
     }
-    operand1 = result.toString();
-    operand2 = '';
-    operator = '';
-    result.textContent = operand1;
   }
-  if (op !== '=') {
-    operator = op;
+  if (operation !== '=') {
+    operator = operation;
+    display.value = operand1 + operator;
+  }
+  if (operation === '=') {
+    output.textContent = result;
   }
 }
 
@@ -85,7 +76,7 @@ function clearDisplay() {
   operand2 = '';
   operator = '';
   result = null;
-  expression = [];
+  output.textContent = '-';
   display.value = '';
 }
 
@@ -93,8 +84,12 @@ function deleteDigit() {
   if (operator === '') {
     operand1 = operand1.slice(0, -1);
     display.value = operand1;
-  } else {
+  } else if (operand2 === '') {
+    operator = '';
+    display.value = operand1 + operator;
+  }
+  else {
     operand2 = operand2.slice(0, -1);
-    display.value = operand2;
+    display.value = operand1 + operator + operand2;
   }
 }
